c18b8e51dcac99edcdc6d4a3d6aebd9ae1b7ee5a
Himanshu Jaroli <54548986+HJ1X@users.noreply.github.com>
2024-01-23 19:24:31 +0530

    move search bar in next row for small devices

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,10 @@
-import { Box, HStack, Image, useColorMode } from "@chakra-ui/react";
+import {
+  Box,
+  HStack,
+  Image,
+  useBreakpoint,
+  useColorMode,
+} from "@chakra-ui/react";
 import LogoForDarkMode from "../assets/aurora-dark.svg";
 import LogoForLightMode from "../assets/aurora-light.svg";
 import ColorModeSwitch from "./ColorModeSwitch";
@@ -12,20 +18,32 @@ const NAVBAR_HEIGHT = "5em";
 
 const navbar = ({ gameCount }: NavBarProps) => {
   const { colorMode } = useColorMode();
+  const screenSize = useBreakpoint();
+
+  const isSmallDevice = screenSize === "sm" || screenSize === "base";
 
   return (
-    <HStack p={3}>
-      <Image
-        borderRadius={"0.2em"}
-        src={colorMode === "dark" ? LogoForDarkMode : LogoForLightMode}
-        boxSize={NAVBAR_HEIGHT}
-        px={3}
-      />
-      <Box style={{ width: "100%" }} mr={2}>
-        <SearchInput gameCount={gameCount} />
-      </Box>
-      <ColorModeSwitch />
-    </HStack>
+    <Box p={3}>
+      <HStack justify="space-between">
+        <Image
+          borderRadius={"0.2em"}
+          src={colorMode === "dark" ? LogoForDarkMode : LogoForLightMode}
+          boxSize={NAVBAR_HEIGHT}
+          px={3}
+        />
+        {!isSmallDevice && (
+          <Box style={{ width: "100%" }} mr={2}>
+            <SearchInput gameCount={gameCount} />
+          </Box>
+        )}
+        <ColorModeSwitch />
+      </HStack>
+      {isSmallDevice && (
+        <Box style={{ width: "100%" }}>
+          <SearchInput gameCount={gameCount} />
+        </Box>
+      )}
+    </Box>
   );
 };
 
