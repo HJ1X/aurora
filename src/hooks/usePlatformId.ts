331eb2c3a1b99a635cd1857ea7f7852f6b419cfb
Himanshu Jaroli <54548986+HJ1X@users.noreply.github.com>
2024-01-23 10:50:27 +0530

    add defensive check in lookup hooks

--- src/hooks/usePlatformId.ts
+++ src/hooks/usePlatformId.ts
@@ -2,7 +2,7 @@ import usePlatforms from "./usePlatforms";
 
 const usePlatformId = (platformId?: number) => {
   const { data: platforms } = usePlatforms();
-  return platforms.results.find((platform) => platform.id === platformId);
+  return platforms?.results.find((platform) => platform.id === platformId);
 };
 
 export default usePlatformId;
